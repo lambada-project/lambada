d8c29448a6b003d5d68612a3a9f360e1a1356df7
Juan C. Andreu <andreujuanc@gmail.com>
2020-12-24 16:20:51 +0100

    k

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -32,19 +32,19 @@ module.exports = {
     footer: {
       style: 'dark',
       links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
+        // {
+        //   title: 'Docs',
+        //   items: [
+        //     {
+        //       label: 'Style Guide',
+        //       to: 'docs/',
+        //     },
+        //     {
+        //       label: 'Second Doc',
+        //       to: 'docs/doc2/',
+        //     },
+        //   ],
+        // },
         {
           title: 'Community',
           items: [
@@ -65,10 +65,10 @@ module.exports = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
+            // {
+            //   label: 'Blog',
+            //   to: 'blog',
+            // },
             {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
--- docs/sidebars.js
+++ docs/sidebars.js
@@ -16,17 +16,5 @@ module.exports = {
       items: ['aws-config'],
       collapsed: false
     }
-    // {
-    //   type: 'category',
-    //   label: "Guides",
-    //   items: ['doc1', 'doc2', 'doc3', 'mdx'],
-    //   collapsed: false
-    // },
-    // {
-    //   type: 'category',
-    //   label: "Format Reference",
-    //   items: ['doc1', 'doc2', 'doc3', 'mdx'],
-    //   collapsed: false
-    // }
   ],
 };
