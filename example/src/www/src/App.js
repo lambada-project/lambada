8d3d34f44890b1fdbb4ffff12ec894b17830caf3
Juan C. Andreu <andreujuanc@gmail.com>
2021-04-09 21:52:19 +0000

    example

--- example/src/www/src/App.js
+++ example/src/www/src/App.js
@@ -1,22 +1,34 @@
 import { useEffect, useState } from 'react'
+import { useMutation, useQuery } from 'react-query'
+
 import './App.css'
+
+const getTodoItems = fetch('/api/todos').then(x => x.json())
+const newTodoItem = (body) => fetch('/api/todos', { method: "POST", body: JSON.stringify(body) }).then(x => x.json())
 
 
 function App() {
-  const [todos, setTodos] = useState({ items: [] })
-  useEffect(() => {
-    fetch('/api/todos')
-      .then(x => x.json())
-      .then(setTodos)
+  const items = useQuery('todo-items', getTodoItems)
+  const create = useMutation('new-todo-item', newTodoItem)
 
-  }, [])
+  const createItem = (title) => {
+    create.mutate({
+      title: title,
+      completed: false
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <ul>
-          {todos.items.map(x => (<li key={x.id}> {x.title}</li>))}
-        </ul>
+        <h2>Lambada to-do list</h2>
       </header>
+      <div className="content">
+        <input onKeyDown={(event) => event.key === "Enter" && createItem(event.target.value)}></input>
+        <ul>
+          {/* {items.map(x => (<li key={x.id}><input checked={x.completed} /> {x.title}</li>))} */}
+        </ul>
+      </div>
     </div>
   );
 }
--- example/src/www/src/index.js
+++ example/src/www/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const client = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client} >
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
