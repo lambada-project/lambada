5d18981e594c6f85c0cb54fcbcf3b3a0ce3054b4
Juan C. Andreu <andreujuanc@gmail.com>
2021-04-09 22:23:26 +0000

    refactored

--- example/src/www/src/App.js
+++ example/src/www/src/App.js
@@ -3,12 +3,12 @@ import { useMutation, useQuery } from 'react-query'
 
 import './App.css'
 
-const getTodoItems = fetch('/api/todos').then(x => x.json())
+const getTodoItems = () => fetch('/api/todos').then(x => x.json())
 const newTodoItem = (body) => fetch('/api/todos', { method: "POST", body: JSON.stringify(body) }).then(x => x.json())
 
 
 function App() {
-  const items = useQuery('todo-items', getTodoItems)
+  const { data, isLoading } = useQuery('todo-items', getTodoItems)
   const create = useMutation('new-todo-item', newTodoItem)
 
   const createItem = (title) => {
@@ -18,17 +18,21 @@ function App() {
     })
   }
 
+
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Lambada to-do list</h2>
+        <h2>My TO-DOs</h2>
       </header>
       <div className="content">
-        <input onKeyDown={(event) => event.key === "Enter" && createItem(event.target.value)}></input>
+        <input type="text" onKeyDown={(event) => event.key === "Enter" && createItem(event.target.value)}></input>
         <ul>
-          {/* {items.map(x => (<li key={x.id}><input checked={x.completed} /> {x.title}</li>))} */}
+          {data?.items.map(x => (<li key={x.id}><input type="checkbox" checked={x.completed} /> {x.title}</li>))}
         </ul>
       </div>
+      <footer style={{ marginTop: '1rem' }}>
+        Powered by <a href="https://lambada-project.github.io">Lambada</a>
+      </footer>
     </div>
   );
 }
