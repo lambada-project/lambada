9eb1807396d050a36b06a8ea9ef2bad9b1213110
Juan C. Andreu <andreujuanc@gmail.com>
2022-02-07 22:56:53 +0000

    cleanup

--- packages/core/src/notifications/index.ts
+++ packages/core/src/notifications/index.ts
@@ -1,6 +1,4 @@
-import * as AWS from "aws-sdk"
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 import * as pulumi from "@pulumi/pulumi";
 
 export type NotificationConfig = {
