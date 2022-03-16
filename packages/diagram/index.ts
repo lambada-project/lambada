2ddcb52ebd6925d99ab13485c4a0b1be342ebde7
Juan C. Andreu <andreujuanc@gmail.com>
2022-03-16 13:03:02 +0000

    demo

--- packages/diagram/index.ts
+++ packages/diagram/index.ts
@@ -1,31 +1,104 @@
 import { FunctionConfiguration, LambdaClient, ListFunctionsCommand } from '@aws-sdk/client-lambda'
-import { GetResourcesCommand, GetResourcesInput, ResourceGroupsTaggingAPIClient, ResourceTagMapping } from '@aws-sdk/client-resource-groups-tagging-api'
+import { SNSClient, ListTopicsCommand, Topic, ListSubscriptionsCommand, Subscription } from '@aws-sdk/client-sns'
+import { GetResourcesCommand, ResourceGroupsTaggingAPIClient, ResourceTagMapping } from '@aws-sdk/client-resource-groups-tagging-api'
 
-const lambdaClient = new LambdaClient({ region: "eu-west-1" });
-const resourcesClient = new ResourceGroupsTaggingAPIClient({ region: "eu-west-1" });
-
+const region = 'eu-west-1'
+const lambdaClient = new LambdaClient({ region: region });
+const resourcesClient = new ResourceGroupsTaggingAPIClient({ region: region });
+const snsClient = new SNSClient({ region: region });
 
 
 
 const main = async () => {
     const lambdas = await getAllLambdas()
+    const topics = await getAllSNSTopics()
+    const subscriptions = await getSubscriptions()
     const tags = await getAllTags("Environment", "dev")
-    const handlers = await getMessageHandlers(tags, lambdas)
-    console.log(handlers)
+
+
+    const events = await getEvents(tags, lambdas, topics, subscriptions)
+
+    generateDiagramData(events)
 }
 
 main()
 
-async function getMessageHandlers(tags: ResourceTagMapping[], lambdas: FunctionConfiguration[]) {
-    return tags
-        .map(x => {
-            return {
-                resource: lambdas.find(y => y.FunctionArn?.toLowerCase() === x.ResourceARN?.toLowerCase()),
-                tags: x.Tags
-            }
-        })
-        .filter(x => x.resource)
+type Event = { subscription: Subscription, handler: FunctionConfiguration, sender: string }
+function generateDiagramData(events: Event[]) {
+    let data = ''
+    for (const e of events) {
+        data+= `${e.sender}->${e.handler.FunctionName} ${e.subscription.TopicArn}\n`;
+    }
+    console.log(data)
 }
+
+function getEvents(tags: ResourceTagMapping[], lambdas: FunctionConfiguration[], topics: Topic[], subscriptions: Subscription[]): Event[] {
+    return subscriptions
+        .map(subscription => ({
+            subscription: subscription.TopicArn?.includes('lockReverted-dev') ? subscription : undefined,
+            handler: lambdas.find(lambda => lambda.FunctionArn == subscription.Endpoint),
+            sender: 'SOMEONE'
+        }))
+        .filter((x): x is Event => x.subscription !== undefined && x.handler !== undefined)
+}
+
+
+
+
+
+async function* getSubscriptionsPage() {
+    let marker: string | undefined
+    do {
+        const listCommand = new ListSubscriptionsCommand({
+            NextToken: marker
+        })
+        const result = await snsClient.send(listCommand)
+        yield result.Subscriptions ?? []
+        marker = result.NextToken
+    } while (marker)
+}
+
+async function getSubscriptions() {
+    const subscriptions: Subscription[] = []
+
+    for await (const page of getSubscriptionsPage()) {
+        subscriptions.push(...page)
+    }
+
+    console.log('Found subscriptions:', subscriptions.length)
+    return subscriptions
+}
+
+
+
+
+
+
+async function* getTopicPage() {
+    let marker: string | undefined
+    do {
+        const listCommand = new ListTopicsCommand({
+            NextToken: marker,
+        })
+        const result = await snsClient.send(listCommand)
+        yield result.Topics ?? []
+        marker = result.NextToken
+    } while (marker)
+}
+
+async function getAllSNSTopics() {
+    const topics: Topic[] = []
+
+    for await (const page of getTopicPage()) {
+        topics.push(...page)
+    }
+
+    console.log('Found topics:', topics.length)
+    return topics
+}
+
+
+
 
 
 async function* getTagPage(key: string, value: string) {
@@ -56,6 +129,9 @@ async function getAllTags(key: string, value: string) {
     console.log('Found tags:', tags.length)
     return tags
 }
+
+
+
 
 
 async function* getLambdaPage() {
